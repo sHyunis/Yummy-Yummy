f92d310d7af7fd2e126fd32775a4d07f4e2d58a4
ayoung <jo.ayoung.1210@gmail.com>
2024-08-30 02:18:43 +0900

    style: Material Symbols 폰트 추가, 프로필, nav, 제목 / feat: mypage 페이지 이동, nav

---
+++ src/pages/mypage/MyPageComment.jsx
@@ -0,0 +1,11 @@
+import MyPageTitle from "./MyPageTitle";
+
+const MyPageComment = () => {
+  return (
+    <>
+      <MyPageTitle>내가 작성한 댓글</MyPageTitle>
+    </>
+  );
+};
+
+export default MyPageComment;
---
+++ src/pages/mypage/MyPagePost.jsx
@@ -0,0 +1,11 @@
+import MyPageTitle from "./MyPageTitle";
+
+const MyPagePost = () => {
+  return (
+    <>
+      <MyPageTitle>내가 작성한 게시글</MyPageTitle>
+    </>
+  );
+};
+
+export default MyPagePost;
