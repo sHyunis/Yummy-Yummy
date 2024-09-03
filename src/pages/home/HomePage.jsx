739ae3800b8ab4ff494e990b2d6e51b98d275378
ayoung <jo.ayoung.1210@gmail.com>
2024-09-03 12:26:06 +0900

    style: home페이지 input, button

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,49 +1,44 @@
 import styled from "styled-components";
-import PostList from "./PostList";
 import { useState } from "react";
-
-const Wrap = styled.div`
-  max-width: 120rem;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-`;
+import Input from "../../components/Input";
+import PostList from "./PostList";
 
 const SearchbarWrap = styled.div`
   position: relative;
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
   margin: 0 auto 50px;
-`;
-
-const Searchbar = styled.input`
-  width: 37rem;
-  height: 4.8rem;
-  padding: 1.2rem 4.8rem 1.2rem 2.4rem;
-  border: 1px solid var(--green-color);
-  border-radius: 24px;
+  input {
+    padding-left: 48px;
+    border-width: 2px;
+    border-color: var(--yellow-color);
+    background-color: var(--beige-color);
+    transition: all ease 0.2s;
+  }
 `;
 
 const SearchICon = styled.div`
   position: absolute;
-  right: 1.2rem;
+  left: 14px;
   top: 50%;
   transform: translateY(-50%);
+  color: var(--yellow-color);
 `;
 
 const HomePage = () => {
   const [inputValue, setInputValue] = useState("");
 
   return (
-    <Wrap>
+    <div className="container">
       <SearchbarWrap>
-        <Searchbar
-          placeholder="검색어를 입력하세요"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
+        <Input placeholder="검색어를 입력하세요." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
         <SearchICon className="material-symbols-rounded">search</SearchICon>
       </SearchbarWrap>
       <PostList keyword={inputValue} />
-    </Wrap>
+    </div>
   );
 };
 
